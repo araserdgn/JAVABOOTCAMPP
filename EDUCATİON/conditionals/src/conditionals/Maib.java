0f970b3edcd1641ce2dfc1585ed705fcbefb085c
İsmail ERDOĞAN <113991303+isoreus@users.noreply.github.com>
2022-10-05 23:38:06 +0300

    Education2

---
+++ EDUCATİON/conditionals/src/conditionals/Maib.java
@@ -0,0 +1,20 @@
+package conditionals;
+
+public class Maib {
+
+	public static void main(String[] args) {
+		int sayi=24;
+		
+		if(sayi<20) {
+			System.out.println("Sayı 20 den kucuktur");
+		}
+		else if(sayi==20) {
+			System.out.println("Sayı 20'ye eşittir.");
+		}
+		else {
+			System.out.println("Sayı 20 den buyuktur");
+		}
+
+	}
+
+}
