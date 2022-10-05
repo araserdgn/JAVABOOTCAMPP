85a5825ad2805fd2a118c2f20f4175f043984dfa
İsmail ERDOĞAN <113991303+isoreus@users.noreply.github.com>
2022-10-05 23:37:16 +0300

    Education1

---
+++ EDUCATİON/veriable/src/veriable/Main.java
@@ -0,0 +1,19 @@
+package veriable;
+
+public class Main {
+	
+	public static void main(String[] args) {
+	int ogrenciSayısı=12;
+	String mesaj = "Ögrenci sayısı : ";
+	
+	
+	
+	System.out.println(mesaj + ogrenciSayısı);
+	
+	System.out.println("ogrenci sayım : "+ogrenciSayısı);
+	System.out.println("ogrenci sayım : "+ogrenciSayısı);
+	System.out.println("ogrenci sayım : "+ogrenciSayısı);
+	System.out.println("ogrenci sayım : "+ogrenciSayısı);
+	
+}
+}
